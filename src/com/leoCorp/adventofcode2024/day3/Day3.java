d4727423c76bc3cbab33d9562453e25c18bd3f52
leoTostain <leo.tostain@gmail.com>
2024-12-04 16:24:16 +0100

    feat: part2 day3

--- src/com/leoCorp/adventofcode2024/day3/Day3.java
+++ src/com/leoCorp/adventofcode2024/day3/Day3.java
@@ -3,6 +3,7 @@ package com.leoCorp.adventofcode2024.day3;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 public class Day3 {
@@ -13,6 +14,7 @@ public class Day3 {
 
         var content = getStringFromFile(args[0]);
         System.out.println(computeMulFromString(content));
+        System.out.println(computeMulFromStringWithDo(content));
     }
 
     private static String getStringFromFile(String filename) {
@@ -38,5 +40,36 @@ public class Day3 {
                     return Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]);
                 })
                 .sum();
+    }
+
+    private static Integer computeMulFromStringWithDo(String content) {
+        var regex = "(mul\\(\\d{1,3},\\d{1,3}\\))|don't|do";
+        var enabled = true;
+        var sum = 0;
+
+        var matches = Pattern.compile(regex)
+                .matcher(content)
+                .results() // correspondences stream (MatchResult)
+                .map(MatchResult::group)
+                .toList();
+
+        for ( var match : matches) {
+            if (match.equals("do")) {
+                enabled = true;
+                continue;
+            } else if (match.equals("don't")) {
+                enabled = false;
+                continue;
+            }
+
+            if (!enabled) {
+                continue;
+            }
+
+            var length = match.length();
+            var nums = match.substring(4, length - 1).split(",");
+            sum += Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]);
+        }
+        return sum;
     }
 }
